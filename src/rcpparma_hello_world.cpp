66c15a7bac5dc907fd65d7a6fec61c5b7c0735d1
Serguei Sokol <sokol@insa-toulouse.fr>
2020-01-14 10:23:45 +0100

    added LinkingTo: r2sundials, rmumps added Suggests: RcppArmadillo, r2sundials

--- src/rcpparma_hello_world.cpp
+++ src/rcpparma_hello_world.cpp
@@ -2,6 +2,8 @@
 
 // we only include RcppArmadillo.h which pulls Rcpp.h in for us
 #include "RcppArmadillo.h"
+// [[Rcpp::depends(r2sundials)]]
+#include <r2sundials.h>
 
 // via the depends attribute we tell Rcpp to create hooks for
 // RcppArmadillo so that the build process will know what to do
@@ -19,7 +21,7 @@ arma::mat rcpparma_hello_world() {
     arma::mat m1 = arma::eye<arma::mat>(3, 3);
     arma::mat m2 = arma::eye<arma::mat>(3, 3);
 	                     
-    return m1 + 3 * (m1 + m2);
+    return m1 + 3 * (m1 + m2) + CV_SUCCESS;
 }
 
 
